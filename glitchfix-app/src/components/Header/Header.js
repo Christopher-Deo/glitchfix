5f243aa0d1254cbbd2ccbafa15da129c8a1ad038
Christopher Deo <110053896+Christopher-Deo@users.noreply.github.com>
2022-12-02 16:56:57 -0600

    added MockAPI integration

--- glitchfix-app/src/components/Header/Header.js
+++ glitchfix-app/src/components/Header/Header.js
@@ -1,12 +1,20 @@
+// React imports
 import React from 'react'
+// Material UI / Other Dependencies imports
+
+// Local imports
+import MainNav from '../MainNav/MainNav';
 import logo from '../../logo.svg';
 import './Header.css';
 
 const Header = () => {
   return (
+    <>
     <header>
       <img className="logo" src= { logo} alt="GlitchFix Logo" />
-    </header>
+      </header>
+    <MainNav />
+      </>
   )
 }
 
